a81f6e120b133eaa8b26bc3dc07f9aaf71322fd2
Michalis Christou <Michalis.Christou@PerkinElmer.com>
2022-02-07 15:59:46 +0200

    first try

--- MyGrammar.java
+++ MyGrammar.java
@@ -1,9 +1,66 @@
+import java.util.ArrayList;
+
 import computation.contextfreegrammar.*;
 
 public class MyGrammar {
 	public static ContextFreeGrammar makeGrammar() {
 		// You can write your code here to make the context-free grammar from the assignment
+		Variable S = new Variable('S');
+		Variable E = new Variable('E');
+		Variable Eone = new Variable('R'); //E1 -> R
+		Variable Etwo = new Variable('Y'); //E2 -> Y
+		Variable A = new Variable('A');
+		Variable B = new Variable('B');
+		Variable C = new Variable('C');
+		Variable D = new Variable('D');
+		Variable F = new Variable('F');
+		Variable T = new Variable('T');
 
-		return null;
+		Terminal one = new Terminal('1');
+		Terminal zero = new Terminal('0');
+		Terminal x = new Terminal('x');
+		Terminal add = new Terminal('+');
+		Terminal mult = new Terminal('*');
+		Terminal sub = new Terminal('-');
+
+		
+		ArrayList<Rule> rules = new ArrayList<>();
+		rules.add(new Rule(S, new Word(E, Eone)));
+		rules.add(new Rule(S, new Word(T, Etwo)));
+		rules.add(new Rule(S, new Word(D, C)));
+		rules.add(new Rule(S, new Word(one)));
+		rules.add(new Rule(S, new Word(zero)));
+		rules.add(new Rule(S, new Word(x)));
+
+		rules.add(new Rule(E, new Word(E, Eone)));
+		rules.add(new Rule(E, new Word(T, Etwo)));
+		rules.add(new Rule(E, new Word(D, C)));
+		rules.add(new Rule(E, new Word(one)));
+		rules.add(new Rule(E, new Word(zero)));
+		rules.add(new Rule(E, new Word(x)));
+
+		rules.add(new Rule(Eone, new Word(A,T)));
+		rules.add(new Rule(Etwo, new Word(B,F)));
+
+		rules.add(new Rule(T, new Word(T, Etwo)));
+		rules.add(new Rule(T, new Word(D, C)));
+		rules.add(new Rule(T, new Word(one)));
+		rules.add(new Rule(T, new Word(zero)));
+		rules.add(new Rule(T, new Word(x)));
+
+		rules.add(new Rule(F, new Word(D, C)));
+		rules.add(new Rule(F, new Word(one)));
+		rules.add(new Rule(F, new Word(zero)));
+		rules.add(new Rule(F, new Word(x)));
+
+		rules.add(new Rule(A, new Word(add)));
+		rules.add(new Rule(B, new Word(mult)));
+		rules.add(new Rule(C, new Word(one)));
+		rules.add(new Rule(C, new Word(zero)));
+		rules.add(new Rule(C, new Word(x)));
+		rules.add(new Rule(D, new Word(sub)));
+
+		ContextFreeGrammar cfg = new ContextFreeGrammar(rules);
+		return cfg;
 	}
 }
--- Parser.java
+++ Parser.java
@@ -3,9 +3,95 @@ import computation.parser.*;
 import computation.parsetree.*;
 import computation.derivation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Parser implements IParser {
   public boolean isInLanguage(ContextFreeGrammar cfg, Word w){
-    return false;
+    try{
+      int lengthOfWord = w.length(), totalDerivations;
+      totalDerivations = (lengthOfWord == 0) ? 1 : (2*lengthOfWord) - 1 ;
+      List<Derivation> allDerivations = Derivations(cfg, totalDerivations);
+      for (Derivation derivation: allDerivations) {
+        if (w.equals(derivation.getLatestWord())) {
+          return true;
+        }
+      } return false;
+    } catch (Exception e){
+        System.out.println("Error in isInLanguage : " + e);
+        return false;
+    }
+  }
+
+  private List<Rule> ruleFinder(Symbol s, ContextFreeGrammar cfg){
+    List<Rule> returnTheseRules = new ArrayList<Rule>();
+    try{
+      //check if r is Terminal
+      if (s.isTerminal()){
+        return returnTheseRules; //escape method
+      }
+      List<Rule> rules = cfg.getRules();
+      for (Rule rule: rules) {
+        if (rule.getVariable().equals(s)) {
+          returnTheseRules.add(rule);
+        }
+      }
+      return returnTheseRules;
+    } catch (Exception e){
+      System.out.println("Error in ruleFinder method : " + e );
+      return returnTheseRules;
+    }
+  }
+  
+  private List<Derivation> Derivations(ContextFreeGrammar cfg, int n) {
+    List<Derivation> returnDerivations = new ArrayList<Derivation>();
+    try{
+      Variable stVar = cfg.getStartVariable();
+      Derivation preDerivation = new Derivation(new Word(stVar));
+      returnDerivations.add(preDerivation);
+
+      List<Derivation> derivations = new ArrayList<Derivation>();
+      for (int i=0;i<n;i++){
+        for(Derivation derivation: derivations){
+          returnDerivations.add(derivation);
+        }
+        derivations = new ArrayList<Derivation>();
+
+        //clone derivation in case it is required for branching
+        //later
+        for(Derivation derivation:returnDerivations){
+          Derivation derivationCopy = new Derivation(derivation);
+          Word w = derivation.getLatestWord();
+          int indexOfWord = 0; boolean stop = false;
+          for(Symbol s:w){
+            if(s.isTerminal()){
+              indexOfWord++;
+              continue;
+            }
+          for(Rule rule:ruleFinder(s, cfg)){
+            Word wordExpansion = rule.getExpansion();
+            Word wordReplaced = w.replace(indexOfWord,wordExpansion);
+            if(stop){
+              Derivation newDerivation = new Derivation(derivationCopy);
+              newDerivation.addStep(wordReplaced,rule,indexOfWord);
+              derivations.add(newDerivation);
+            } else {
+              derivation.addStep(wordReplaced, rule, indexOfWord);
+              stop = true;
+            }
+          }
+          indexOfWord++;
+          } 
+        }
+      }
+      for(Derivation derivation: derivations){
+        returnDerivations.add(derivation);
+      }
+      return returnDerivations;
+    }catch (Exception e){
+      System.out.println("Error in Derivations : " + e);
+      return returnDerivations;
+    }
   }
 
   public ParseTreeNode generateParseTree(ContextFreeGrammar cfg, Word w) {
